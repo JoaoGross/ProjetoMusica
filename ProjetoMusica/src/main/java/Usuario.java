f538b9a987b268c615f603590ef4dde4eab97c8c
Joao V. Gross <70983696+JoaoGross@users.noreply.github.com>
2020-11-13 16:31:38 -0300

    Update Usuario.java

--- ProjetoMusica/src/main/java/Usuario.java
+++ ProjetoMusica/src/main/java/Usuario.java
@@ -12,6 +12,11 @@ public class Usuario {
 		this.senha = senha;
 		this.generosPreferidos = generosPreferidos;
 		//armazenar no banco de dados
+		ConnectionFactory.salvarUsuario(nomeUsuario, senha);
+		this.IdUsuario = ConnectionFactory.pegarIdUsuario(nomeUsuario);
+		for(int i = 0; i<= generosPreferidos.size(); i++) {
+			ConnectionFactory.guardarGeneroPreferido(generosPreferidos.get(i).getId(), IdUsuario);
+		}
 	}
 	//proximos logins
 	public Usuario(String nomeUsuario, String senha){

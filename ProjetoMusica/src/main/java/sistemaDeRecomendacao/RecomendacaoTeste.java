8f6878ac75be381b2076a0fa4a36bcf0bf854b50
Joao V. Gross <Joao V. Gross@DESKTOP-1V8CE66>
2020-11-21 16:46:08 -0300

    Atualizacao nas telas

--- ProjetoMusica/src/main/java/sistemaDeRecomendacao/RecomendacaoTeste.java
+++ ProjetoMusica/src/main/java/sistemaDeRecomendacao/RecomendacaoTeste.java
@@ -2,8 +2,11 @@ package sistemaDeRecomendacao;
 
 import javax.swing.JOptionPane;
 
+import sistemaDeRecomendacao.telas.TelaLogin;
+
 public class RecomendacaoTeste {
 	public static void main(String args[]) {
-		
+		new TelaLogin().setVisible(true);
+		 
 	}
 }
